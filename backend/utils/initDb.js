ced183da94c88ae54ccd81cf7ce9444e7410eb60
Jack <jack.scotson@netacea.com>
2019-10-26 20:19:47 +0100

    Auto stash before merge of "master" and "origin/master"

--- backend/utils/initDb.js
+++ backend/utils/initDb.js
@@ -7,13 +7,15 @@ mongoose.connect('mongodb://localhost/scanner', { useNewUrlParser: true })
 
 async.parallel([
     (cb) => { Product.deleteMany({}, cb) },
-    (cb) => { new Product({ id : "0", slug : 'banana', name: "The Big Banana", price: 0.95, quantity: 10 }).save(cb) },
-    (cb) => { new Product({ id : "1", slug : 'phone', name: "The New zPhone X-Treme", price: 965, quantity: 3 }).save(cb) },
-    (cb) => { new Product({ id : "2", slug : 'pen', name: "High Quality Pen Island Pen", price: 0.35, quantity: 100 }).save(cb) },
-    (cb) => { new Product({ id : "3", slug : 'jack', name: "Jack Scotson, Principal Developer", price: 99999.99, quantity: 1 }).save(cb) },
-    (cb) => { new Product({ id : "4", slug : 'chair', name: "Bean Bag Chair", price: 20.50, quantity: 4 }).save(cb) },
-    (cb) => { new Product({ id : "5", slug : 'stressball', name: "MongoDB Branded Stress Ball", price: 2.45, quantity: 3 }).save(cb) },
-    (cb) => { new Product({ id : "6", slug : 'pie', name: "Ratio of Circumference and Diameter of a Circle", price: 3.14, quantity: 99 }).save(cb) },
+    (cb) => { new Product({ id : '000000000048', slug : 'banana', name: "The Big Banana", price: 0.95, quantity: 10 }).save(cb) },
+    (cb) => { new Product({ id : '000000000055', slug : 'phone', name: "The New zPhone X-Treme", price: 965, quantity: 3 }).save(cb) },
+    (cb) => { new Product({ id : '000000000062', slug : 'pen', name: "High Quality Pen Island Pen", price: 0.35, quantity: 100 }).save(cb) },
+    (cb) => { new Product({ id : '000000000079', slug : 'jack', name: "Jack Scotson, Principal Developer", price: 99999.99, quantity: 1 }).save(cb) },
+    (cb) => { new Product({ id : '000000000086', slug : 'chair', name: "Bean Bag Chair", price: 20.50, quantity: 4 }).save(cb) },
+    (cb) => { new Product({ id : '000000000093', slug : 'stressball', name: "MongoDB Branded Stress Ball", price: 2.45, quantity: 3 }).save(cb) },
+    (cb) => { new Product({ id : '000000000109', slug : 'pie', name: "Ratio of Circumference and Diameter of a Circle", price: 3.14, quantity: 99 }).save(cb) },
+    (cb) => { new Product({ id : '8714789810348', slug : 'colgate', name: "Colgate", price: 0.88, quantity: 99 }).save(cb) },
+    (cb) => { new Product({ id : '8714789810348', slug : 'cadburybrunchbar', name: "Cadbury\'s brunch bar", price: 0.88, quantity: 99 }).save(cb) },
     (cb) => { Session.deleteMany({}, cb) }
 ], (err) => {
     if (err) {
