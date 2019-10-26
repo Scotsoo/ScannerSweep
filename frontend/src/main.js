eba41f49019a32d3a971f1edf3d0b81e2c69f97c
Jack <jack.scotson@netacea.com>
2019-10-26 15:39:35 +0100

    include vue router

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
+import Home from './components/Home'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/', component: Home, name: 'Home'
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router,
+  template: '<App/>',
 }).$mount('#app')
