c43ec9e1148724f41b2c990b1c9b77a41eef169e
Mindez <mindez@gmail.com>
2019-10-26 20:18:01 +0100

    changed product id to be a string

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose')
 
 const productSchema = new mongoose.Schema({
-    id : { type : Number },
+    id : { type : String },
     slug : { type : String },
     name : { type : String },
     price : { type : Number },
